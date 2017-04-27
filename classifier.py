dc1bef8b5bf8392fa19cf66612a3c871ead3ff6f
Eva Balog <evebalog@netscape.net>
2017-04-27 13:46:54 +0100

    Create featuresets

--- classifier.py
+++ classifier.py
@@ -17,8 +17,20 @@ for w in movie_reviews.words():
 all_words = nltk.FreqDist(all_words)
 
 word_features = list(all_words.keys())[:3000]
-print(word_features)
-#
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+
 # print(all_words["enticing"])
 
 # print(len(all_words))
